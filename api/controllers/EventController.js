f9cf3fd82df8ca7f3307bd51791d8ef53989177b
404nullll <18416144@life.hkbu.edu.hk>
2018-10-22 00:39:57 +0800

    Finish FrontEnd

--- api/controllers/EventController.js
+++ api/controllers/EventController.js
@@ -6,7 +6,55 @@
  */
 
 module.exports = {
-  
+
+  // index
+  index: async function (req, res) {
+
+    return res.view('pages/index');
+
+  },
+
+  // detail
+  detail: async function (req, res) {
+
+    return res.view('pages/detail');
+
+  },
+
+  // create
+  create: async function (req, res) {
+
+    return res.view('pages/create');
+
+  },
+
+  // admin
+  admin: async function (req, res) {
+
+    return res.view('pages/admin');
+
+  },
+
+  // update
+  update: async function (req, res) {
+
+    return res.view('pages/update');
+
+  },
+
+  // delete
+  delete: async function (req, res) {
+
+    return res.view('pages/delete');
+
+  },
+
+  // search
+  search: async function (req, res) {
+
+    return res.view('pages/search');
+
+  },
 
 };
 
--- config/routes.js
+++ config/routes.js
@@ -10,50 +10,20 @@
 
 module.exports.routes = {
 
+  '/' : 'EventController.index',
 
-  //  ╦ ╦╔═╗╔╗ ╔═╗╔═╗╔═╗╔═╗╔═╗
-  //  ║║║║╣ ╠╩╗╠═╝╠═╣║ ╦║╣ ╚═╗
-  //  ╚╩╝╚═╝╚═╝╩  ╩ ╩╚═╝╚═╝╚═╝
+  '/detail' : 'EventController.detail',
 
-  /***************************************************************************
-  *                                                                          *
-  * Make the view located at `views/homepage.ejs` your home page.            *
-  *                                                                          *
-  * (Alternatively, remove this and add an `index.html` file in your         *
-  * `assets` directory)                                                      *
-  *                                                                          *
-  ***************************************************************************/
+  '/create' : 'EventController.create',
 
-  '/': {
-    view: 'pages/homepage'
-  },
+  '/update' : 'EventController.update',
 
-  /***************************************************************************
-  *                                                                          *
-  * More custom routes here...                                               *
-  * (See https://sailsjs.com/config/routes for examples.)                    *
-  *                                                                          *
-  * If a request to a URL doesn't match any of the routes in this file, it   *
-  * is matched against "shadow routes" (e.g. blueprint routes).  If it does  *
-  * not match any of those, it is matched against static assets.             *
-  *                                                                          *
-  ***************************************************************************/
+  '/admin' : 'EventController.admin',
 
+  '/search' : 'EventController.search'
 
-  //  ╔═╗╔═╗╦  ╔═╗╔╗╔╔╦╗╔═╗╔═╗╦╔╗╔╔╦╗╔═╗
-  //  ╠═╣╠═╝║  ║╣ ║║║ ║║╠═╝║ ║║║║║ ║ ╚═╗
-  //  ╩ ╩╩  ╩  ╚═╝╝╚╝═╩╝╩  ╚═╝╩╝╚╝ ╩ ╚═╝
-
-
-
-  //  ╦ ╦╔═╗╔╗ ╦ ╦╔═╗╔═╗╦╔═╔═╗
-  //  ║║║║╣ ╠╩╗╠═╣║ ║║ ║╠╩╗╚═╗
-  //  ╚╩╝╚═╝╚═╝╩ ╩╚═╝╚═╝╩ ╩╚═╝
-
-
-  //  ╔╦╗╦╔═╗╔═╗
-  //  ║║║║╚═╗║
-  //  ╩ ╩╩╚═╝╚═╝
-
+  // '/': {
+  //   view: 'pages/homepage'
+  // },
 
 };
